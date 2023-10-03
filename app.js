4beff96705945288c65af81d63d5ebde6990c983
Sweattowel <sirflaminghorse@hotmail.com>
2023-10-03 13:25:35 +1000

    C U B E
    C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E C U B E

--- app.js
+++ app.js
@@ -12,23 +12,29 @@ function setPage(a){
                         </p>
                     </div>
                 </div>
-                <div id="display">
+                <div class="display">
                     <img src="images/124-3504x2336.jpg" alt="Boat in ocean" srcset="">
                 </div> 
             </div>
             <img id="homeImage" src="images/133-2742x1828.jpg" alt="Two parked cars" srcset="">`
         break;
+        // why the fuck does the code not find myCanvas its the same fucking thing 
         case 2: document.getElementById('bodyH').innerHTML =`
+           
+                <div id="information">
+                    <h1>Enjoy our everchanging catalogue </h1>
+                </div>
+            
             <div>
                 <div id="information">
                     Elevate your project with this tranquil scene — a greyish hill overlooking a meandering river. Muted tones create a calming backdrop, perfect for conveying serenity and untouched landscapes.
                 </div>
-                <div id="display">
+                <div class="display">
                     <img src="images/125-1500x1000.jpg" alt="Hill overlooking river" srcset="">
                 </div>
             </div>
             <div>
-                <div id="display" style="border-left: none; border-right: 2px solid white;">
+                <div class="display" style="border-left: none; border-right: 2px solid white;">
                     <img src="images/127-4032x2272.jpg" alt="Forest" srcset=""style="border-left: none;">
                 </div>
                 <div id="information">
@@ -39,7 +45,7 @@ function setPage(a){
                 <div id="information">
                     Transport your audience with this breathtaking shot — from grass to a mountain-framed lake. Perfect for projects conveying adventure and the awe-inspiring grandeur of landscapes.
                 </div>
-                <div id="display">
+                <div class="display">
                 <img src="images/128-3823x2549.jpg" alt="Grass overlooking lake" srcset="">
                 </div>
             </div>`
@@ -69,3 +75,6 @@ function setPage(a){
     
     }
 }
+
+
+
