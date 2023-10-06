905468c7b1d9051a09df0ab95004b040dbce49e8
Sweattowel <sirflaminghorse@hotmail.com>
2023-10-06 19:31:16 +1000

    updating explorer

---
+++ INDEX.js
@@ -0,0 +1,11 @@
+function chooseSite(a) {
+    switch(a){
+        case 1:
+            location.replace('https://sweattowel.github.io/Employee_tracker/index.html/')
+        break;
+        case 2:
+            location.replace('https://sweattowel.github.io/Stock_Photography/index.html/')
+        break;
+
+    }
+}
