9a5e8412b8b6295ee8c4820bc5b3025e4c516ec9
Sweattowel <sirflaminghorse@hotmail.com>
2023-10-06 20:03:21 +1000

    actuaally update pretty please

--- INDEX.js
+++ INDEX.js
@@ -1,6 +1,7 @@
 function chooseSite(a) {
     switch(a){
         case 1:
+            // wouldnt update normally for some reason attempting to force via random comment
             location.replace('https://sweattowel.github.io/Employee_tracker/index.html')
         break;
         case 2:
