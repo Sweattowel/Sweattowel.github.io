91755dfeaea4a3c0b731388ed68b87b0275965ae
Sweattowel <sirflaminghorse@hotmail.com>
2023-10-06 19:55:31 +1000

    small

--- INDEX.js
+++ INDEX.js
@@ -1,10 +1,10 @@
 function chooseSite(a) {
     switch(a){
         case 1:
-            location.replace('https://sweattowel.github.io/Employee_tracker/index.html/')
+            location.replace('https://sweattowel.github.io/Employee_tracker/index.html')
         break;
         case 2:
-            location.replace('https://sweattowel.github.io/Stock_Photography/index.html/')
+            location.replace('https://sweattowel.github.io/Stock_Photography/index.html')
         break;
 
     }
